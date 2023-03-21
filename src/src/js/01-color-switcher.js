dee927c3891f4e3967b976fe36a7f3039904b935
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-21 22:12:46 +0100

    Update 01-color-switcher.js

--- src/src/js/01-color-switcher.js
+++ src/src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+// startBtn.addEventListener('click', ()=>{
+    
+// })
