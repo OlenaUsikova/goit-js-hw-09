c58679a3de84dd0dda6b3b806aa8d51fdef9c9b6
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-24 13:05:52 +0100

    task3-markup

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,70 +1,80 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const inputDate = document.querySelector('#datetime-picker')
-const startBtn = document.querySelector('button')
-const daysField = document.querySelector('data[days]')
-const hoursField = document.querySelector('data[hours]')
-const minutesField = document.querySelector('data[minutes]')
-const secondsField = document.querySelector('data[seconds]')
+const inputDate = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button');
+const daysField = document.querySelector('[data-days]');
+const hoursField = document.querySelector('[data-hours]');
+const minutesField = document.querySelector('[data-minutes]');
+const secondsField = document.querySelector('[data-seconds]');
+console.log(daysField);
 
+startBtn.setAttribute('disabled', true);
 
-startBtn.setAttribute('disabled', true)
-
-let interval = null
-let ourDate = 0
+let interval = null;
+let ourDate = 0;
 
 // const fp = flatpickr(inputDate, {});
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0])
-      ourDate = selectedDates[0]
-      if(selectedDates[0] > new Date()){
-        startBtn.disabled = false;
-      } else {window.alert("Please choose a date in the future")}
-    }}
-    
-    flatpickr(inputDate, options)
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    ourDate = selectedDates[0];
+    if (selectedDates[0] > new Date()) {
+      startBtn.disabled = false;
+    } else {
+    //   window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+    }
+  },
+};
 
-    function addLeadingZero(value){
-        return String(value).padStart(2, '0')
-    };
+flatpickr(inputDate, options);
 
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        // Remaining days
-        const days = Math.floor(ms / day);
-        // Remaining hours
-        const hours = addLeadingZero(Math.floor((ms % day) / hour));
-        // Remaining minutes
-        const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-        // Remaining seconds
-        const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-      
-        return { days, hours, minutes, seconds };
-      }
-       const difDates = ourDate - new Date();
-       let arrayDays = convertMs(difDates);
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
-   function insertDates (arrayDays){
-    daysField.textContent = arrayDays.days;
-    hoursField.textContent = arrayDays.hours;
-    minutesField.textContent = arrayDays.minutes;
-    secondsField.textContent = arrayDays.seconds;
-   }
-   startBtn.addEventListener('click', () => {
-    interval = setInterval(() => {
-        insertDates()}, 1000)
-        if(difDates === 0){
-clearInterval(interval)
-        }});
-    
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function insertDates(a) {
+  daysField.innerHTML = a.days;
+  hoursField.innerHTML = a.hours;
+  minutesField.innerHTML = a.minutes;
+  secondsField.innerHTML = a.seconds;
+}
+
+startBtn.addEventListener('click', () => {
+  interval = setInterval(() => {
+    const difDates = ourDate - new Date();
+    let arrayDays = convertMs(difDates);
+    insertDates(arrayDays);
+    console.log(difDates)
+    if (difDates < 1000) {
+      clearInterval(interval);
+    }
+  }, 1000);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+const startBtn = form.querySelector('submit');
+const delayInp = form.querySelector('delay');
+const stepInp = form.querySelector('step');
+const numberInp = form.querySelector('amount');
+
+let delayN = delayInp.addEventListener.target.elements.value;
+let stepN = stepInp.addEventListener.target.elements.value;
+let amountN = numberInp.addEventListener.target.elements.value;
+
 function createPromise(position, delay) {
+  return (new Promise((resolve, reject)) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay})
   } else {
-    // Reject
-  }
+    reject({position, delay})
+  }})
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  })
