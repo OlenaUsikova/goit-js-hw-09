ae6cab45cc8629bbabca1901d51aa5fc469d9f03
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-27 14:45:15 +0000

    Deploying to gh-pages from @ OlenaUsikova/goit-js-hw-09@906ee0a52c9ef5405b654e7b3e4282054484e947 🚀

---
+++ 01-color-switcher.9662bf9a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;function o(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{t.setAttribute("disabled",!0),n=setInterval(o,1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9662bf9a.js.map
